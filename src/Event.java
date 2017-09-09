1b8aee4b68c4bb2ab73119fc067698ac9561f0e3
Emma Dean <emmadeannow@gmail.com>
2017-09-09 02:54:15 -0400

    added clarification to return event action as null if not one of the four options provided. Test for null is throwing proper error.

--- src/Event.java
+++ src/Event.java
@@ -2,25 +2,46 @@ public class Event {
     private static String name;
     private static String action;
 
+    boolean b;
+
 
     Event(){
 
+
+
     }
+
 
     public static String getName() {
         return name;
     }
 
     public static String getAction() {
-        return action;
-    }
+
+        if (action.equals("Face2Face") ||
+                action.equals("PhoneCall") ||
+                action.equals("TextMessaging") ||
+                action.equals("Unknown")){
+
+            return action;
+    }else{
+    return "err";
+    }};
 
     public void setName(String name) {
         this.name = name;
     }
 
     public void setAction(String action) {
-        this.action = action;
+        if (this.action.equals("Face2Face") ||
+                this.action.equals("PhoneCall") ||
+                this.action.equals("TextMessaging") ||
+                this.action.equals("Unknown")){
+            this.action = action;
+        }
+       else{
+            this.action.isEmpty();
+        }
     }
 
 
